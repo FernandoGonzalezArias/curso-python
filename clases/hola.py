1e8401d532a1fdb2c462e39560e683f47a672ab9
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2023-04-19 22:12:51 -0400

    primeri-comit

---
+++ clases/hola.py
@@ -0,0 +1,18 @@
+print("Hola Mundo")
+
+edad  = 28
+sueldo = 1000.50
+nombre = "Fernando"
+isTeacher = False
+menu = 'carne, "pollo - arroz", pure'
+
+print(edad)
+print(sueldo)
+print(nombre)
+print(isTeacher)
+print(menu)
+
+numero_1 = 1
+numero_2 = 3
+resultado = (numero_1 * numero_2) +100
+print(resultado)
