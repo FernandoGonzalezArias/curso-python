935330694af78808dad50dec2a8f94aee1fbb48e
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2023-05-06 13:43:56 -0400

    fin segunda parte

---
+++ Modulo-3-Fundamentos de Programación en Python/ejercicios/ejercicio-12.py
@@ -0,0 +1,16 @@
+# Los alumnos de un curso se han dividido en dos grupos A y B 
+# de acuerdo al sexo y el Nombre. El grupo A esta formado
+# por las mujeres con un nombre anterior a la M
+# Y a los hombres con un nombre posterior a la N y 
+# el grupo B por el resto. Escribir un programa que pregunte al usuario
+# su nombre y sexo, y muestre por pantalla el grupo que le corresponde
+
+nombre = str(input("Ingrese su nombre: "))
+sexo = str(input("Ingrese su sexo (M/H): "))
+
+if (sexo == "M" and nombre < "M") or (sexo == "H" and nombre > "N"):
+    grupo = "A"
+else:
+    grupo = "B"
+
+print(f'Tu nombre es {nombre}, tu sexo es {sexo}, entonces perteneces al grupo {grupo}')
---
+++ Modulo-3-Fundamentos de Programación en Python/ejercicios/ejercicio-14.py
@@ -0,0 +1,33 @@
+# La pizzería Bella Napoli ofrece pizzas vegetarianas y no vegetarianas a sus clientes.
+# Los ingredientes para cada tipo de pizza aparecen a continuación.
+
+# Ingredientes vegetarianos: Pimiento y tofu.
+# Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
+# Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no,
+# y en función de su respuesta le muestre un menú con los ingredientes disponibles
+# para que elija. Solo se puede eligir un ingrediente además de la mozzarella 
+# y el tomate que están en todas la pizzas. Al final se debe mostrar por pantalla 
+# si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
+
+opcion = input("¿Quiere una pizza vegetariana? (Sí/No): ")
+
+if opcion.lower() == "sí":
+    print("Ingredientes vegetarianos disponibles:")
+    print("- Pimiento")
+    print("- Tofu")
+    ingrediente = input("Elija un ingrediente (Pimiento/Tofu): ")
+    es_vegetariana = True
+else:
+    print("Ingredientes no vegetarianos disponibles:")
+    print("- Peperoni")
+    print("- Jamón")
+    print("- Salmón")
+    ingrediente = input("Elija un ingrediente (Peperoni/Jamón/Salmón): ")
+    es_vegetariana = False
+
+print("Usted ha elegido una pizza", end=" ")
+if es_vegetariana:
+    print("vegetariana")
+else:
+    print("no vegetariana")
+print("Ingredientes: Mozzarella, Tomate,", ingrediente)
