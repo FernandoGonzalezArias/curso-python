df6df2cbcf07ed3bca1796a2524fabb40f427eb5
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2023-05-05 21:54:51 -0400

    up

--- Modulo-3-Fundamentos de Programación en Python/ejercicios/ejercicio-3.py
+++ Modulo-3-Fundamentos de Programación en Python/ejercicios/ejercicio-3.py
@@ -1,2 +1,9 @@
+# imprimir un programa  que pregunte 
+# el nombre del usuario en la consola y despues 
+# de que el usuario lo introduzca muestre por pantalla
+# la cadena ¡ Hola <nombre> !, donde <nombre>
+# es el nombre que el usuario haya introducido
+
+
 nombre = input("Por favor, introduce tu nombre: ")
 print("¡Hola", nombre, "!")
