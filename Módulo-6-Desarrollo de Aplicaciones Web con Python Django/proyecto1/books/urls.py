d0d5feb97a0e2bf62ea5323bcd359f9b774f5e8f
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2023-06-29 20:50:52 -0400

    up

--- Módulo-6-Desarrollo de Aplicaciones Web con Python Django/proyecto1/books/urls.py
+++ Módulo-6-Desarrollo de Aplicaciones Web con Python Django/proyecto1/books/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path ('', views.books_list),
+    path('<int:pk>', views.book_detail),  #/book/id
+    path('new', views.new_book)
     
 ]
--- Módulo-6-Desarrollo de Aplicaciones Web con Python Django/proyecto1/books/views.py
+++ Módulo-6-Desarrollo de Aplicaciones Web con Python Django/proyecto1/books/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+from django.http import HttpResponse
+from .models import Book
+from.forms import BookForm
 
 
 def books_list(request):
     books = models.Book.objects.all()
     context = {'books':books}
     return render(request, 'books_list.html', context)
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    context = {'book': book}
+    return render(request, 'books_detail.html', context)
+
+def  new_book(request):
+    if request.method == 'POST':
+        #gusradr nuevo book
+        form = BookForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('/books')
+    else:
+        #ir al formulario
+        form = BookForm()
+        return render(request, 'new_book.html', {'form': form})
